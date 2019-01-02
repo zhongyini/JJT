15126f08c0e45bbbeb40e16763f7f721f00451e9
zhongniyi <1245423598@qq.com>
2019-01-02 23:51:21 +0800

    update

--- xxx_wechat_front/src/main/java/com/xxx/wechat/front/controller/NormalDistributionController.java
+++ xxx_wechat_front/src/main/java/com/xxx/wechat/front/controller/NormalDistributionController.java
@@ -22,7 +22,18 @@ public class NormalDistributionController {
 	
 	@Autowired
 	private INormalDistributionService normalDistributionService;
+	/**
+	 * 获取当期详情
+	 * http://www.lottery.gov.cn/api/lottery_kj_detail_new.jspx?_ltype=4&_term=18153
+	 * 数组eventName、details
+	 * json对象lottery
+	 */
 	
+	/**
+	 * 
+	 * @param numberStr
+	 * @return
+	 */
 	@RequestMapping(value = "/select", method = RequestMethod.GET)
 	public RestResult selectNormalDistribution(String numberStr) {
 		try{
